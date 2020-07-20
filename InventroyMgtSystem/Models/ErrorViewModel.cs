b4e29f127865a8cdba32ef8712a144d3a7817a34
Luqman Nasirudeen <olawale7040@gmail.com>
2020-07-20 18:55:45 +0100

    Add project files.

---
+++ InventroyMgtSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace InventroyMgtSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
